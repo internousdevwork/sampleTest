ee24c65270de0fc886d65468e6f32622a0997e48
インターノウス <@DESKTOP-DFVO5UE>
2019-12-24 13:26:18 +0900

    first commit

---
+++ src/sampleTest/Test.java
@@ -0,0 +1,17 @@
+package sampleTest;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		// start
+
+		// end
+
+		System.out.println("商品の価格：" + price);
+		System.out.println("消費税：" + consumtionTax);
+		System.out.println("合計金額：" + totalPrice);
+
+	}
+
+}
