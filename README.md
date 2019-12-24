59bfcfe9c6797f8db48d1b377dd732b054d0ae79
ykamiki <y.kamiki@internous.co.jp>
2019-12-24 15:32:17 +0900

    consumptionのスペル修正、マークダウンに改行を付与。

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # sampleTest
 350円の商品の消費税(8%)と、消費税を合わせた合計金額を求めてください。
+
 それぞれの変数名は以下のものとする
+
 商品の価格：price,消費税：consumptionTax,合計金額：totalPrice
--- src/sampleTest/Test.java
+++ src/sampleTest/Test.java
@@ -9,7 +9,7 @@ public class Test {
 		// end
 
 		System.out.println("商品の価格：" + price);
-		System.out.println("消費税：" + consumtionTax);
+		System.out.println("消費税：" + consumptionTax);
 		System.out.println("合計金額：" + totalPrice);
 
 	}
