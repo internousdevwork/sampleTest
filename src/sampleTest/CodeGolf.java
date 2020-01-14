4e418d6c0eefee92bff34b93cabcdd75145a4a17
インターノウス <@DESKTOP-DFVO5UE>
2020-01-14 13:46:46 +0900

    コードゴルフ用のファイル追加

---
+++ src/sampleTest/CodeGolf.java
@@ -0,0 +1,23 @@
+package sampleTest;
+
+public class CodeGolf {
+
+	/*
+	 * コードの内容
+	 * 1～100の数字をコンソールに出力するソースコードを作成して下さい。
+	 * ただし3の倍数はFizzを5の倍数はBuzzを表示してください
+	 */
+	public static void main(String[] args) {
+		for (int i=1; i<=100; i++) {
+			if(i % 3 == 0 && i % 5 == 0){
+				System.out.println("FizzBuzz");
+			}else if(i % 3 == 0){
+				System.out.println("Fizz");
+			}else if(i % 5 == 0){
+				System.out.println("Buzz");
+			}else{
+				System.out.println(i);
+			}
+		}
+	}
+}
