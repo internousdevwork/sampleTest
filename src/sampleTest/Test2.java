3dfef71fada0857eebfb4008e94aa35fbf71f6d1
ykamiki <y.kamiki@internous.co.jp>
2020-01-24 15:43:47 +0900

    READMEファイルを問題ごとに新規作成。
    Test2、Test3を追加。

---
+++ src/sampleTest/Test2.java
@@ -0,0 +1,23 @@
+package sampleTest;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+		// start
+
+		// end
+
+	}
+
+	public static /*(ここを埋める)*/ calculate() {
+		// start
+
+		// end
+
+		System.out.println("税抜価格：" + price +"円");
+		System.out.println("消費税：" + consumptionTax + "円");
+		System.out.println("税込価格：" + totalPrice + "円");
+	}
+
+}
